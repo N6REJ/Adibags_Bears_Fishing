2f53c3fa632b6d522fb97086034734fb8fd25ccb
Bear <programming@hallhome.us>
2021-07-19 18:52:21 -0500

    Add files via upload

---
+++ database.lua
@@ -0,0 +1,24 @@
+-- AdiBags_Shadowlands_Crafting - Fish Database
+-- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+--
+local ADDON_NAME, ADDON_TABLE, addon = ...
+
+-- Create addon table
+N = { }
+ADDON_TABLE.N = N
+
+-- Label to use
+N["FilterTitle"] = "Fish"
+
+-- Database of items
+N["database"] = {
+	-- ID,	--Item name
+	173032,	-- Lost Sole
+	173033,	-- Iridescent Amberjack
+	173034,	-- Silvergill Pike
+	173035,	-- Pocked Bonefish
+	173036,	-- Spinefin Piranha
+	173037,	-- Elysian Thade
+	174327,	-- Malformed Gnasher
+}
