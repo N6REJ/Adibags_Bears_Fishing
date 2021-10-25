38bddb9fe398bf59c291087a6518f1d215d0f053
N6REJ <programming@hallhome.us>
2021-10-25 06:07:13 -0500

    WORKS!

--- database.lua
+++ database.lua
@@ -16,33 +16,32 @@ db.Filters = {
 		uiName = "Shadowlands Fish",
 		uiDesc = "Fish found while fishing in Shadowlands",
 		title = "Fish",
+		items = {
+			-- ID,	--Item name
+			[173032] = true,	-- Lost Sole
+			[173033] = true,	-- Iridescent Amberjack
+			[173034] = true,	-- Silvergill Pike
+			[173035] = true,	-- Pocked Bonefish
+			[173036] = true,	-- Spinefin Piranha
+			[173037] = true,	-- Elysian Thade
+			[174327] = true,	-- Malformed Gnasher
+		},
 	},
 	["Bait"] = {
 		uiName = "Shadowlands Bait",
 		uiDesc = "Bait found while fishing in Shadowlands",
 		title = "Bait",
+		items = {
+			-- ID,	--Item name
+		   [173038] = true,	-- Lose Sole bait
+		   [173039] = true,	-- Iridescent amberjack bait
+		   [173040] = true,	-- Silvergill pike bait
+		   [173041] = true,	-- Pocked Bonefish bait
+		   [173042] = true,	-- Spinefin piranha bait
+		   [173043] = true,	-- Elysian thade bait
+	   },
 	},
 }
 
-db.Fish = {
-	-- ID,	--Item name
-	[173032] = true,	-- Lost Sole
-	[173033] = true,	-- Iridescent Amberjack
-	[173034] = true,	-- Silvergill Pike
-	[173035] = true,	-- Pocked Bonefish
-	[173036] = true,	-- Spinefin Piranha
-	[173037] = true,	-- Elysian Thade
-	[174327] = true,	-- Malformed Gnasher
-}
-
-db.Bait = {
-	 -- ID,	--Item name
-	[173038] = true,	-- Lose Sole bait
-	[173039] = true,	-- Iridescent amberjack bait
-	[173040] = true,	-- Silvergill pike bait
-	[173041] = true,	-- Pocked Bonefish bait
-	[173042] = true,	-- Spinefin piranha bait
-	[173043] = true,	-- Elysian thade bait
-}
 -- now that db is populated lets pass it on.
 addonTable.db = db
